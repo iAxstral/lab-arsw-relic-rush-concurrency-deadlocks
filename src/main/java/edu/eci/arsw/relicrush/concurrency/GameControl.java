49c66c56f876fb245c1b799de249d04e07fe63aa
Tomas <tomasdi.dev@gmail.com>
2026-08-21 19:35:04 -0500

    feat: primera clase para el gui

---
+++ src/main/java/edu/eci/arsw/relicrush/concurrency/GameControl.java
@@ -0,0 +1,36 @@
+package edu.eci.arsw.relicrush.concurrency;
+
+public class GameControl {
+    private boolean paused = false;
+    private volatile boolean stopped = false;
+
+    public synchronized void pause() {
+        paused = true;
+    }
+
+    public synchronized void resume() {
+        paused = false;
+        notifyAll();
+    }
+
+    public synchronized void waitIfPaused() throws InterruptedException {
+        while (paused && !stopped) {
+            wait();
+        }
+    }
+
+    public void stop() {
+        stopped = true;
+        synchronized (this) {
+            notifyAll();
+        }
+    }
+
+    public boolean isStopped() {
+        return stopped;
+    }
+
+    public synchronized boolean isPaused() {
+        return paused;
+    }
+}
